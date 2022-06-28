338ea9263b020ca35e8f5e42ab118d57158c8ccc
MrRunix <runix@internet.ru>
2022-06-28 11:55:42 +0300

    v0.1 without screenshots

---
+++ program/client.py
@@ -0,0 +1,54 @@
+import socket
+import threading
+from time import sleep
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#создаём сокет
+sock.bind(('', 0))
+server = '127.0.0.1', 4321 #сервер для подключения
+def choise_name(): # как общаться без имени?
+    while True:
+        xname = input(str('Введите ваш никнейм) \n'))
+        if xname == None:
+            print('Имя есть у каждого, вводи, сцука \n <. \t- _ - \t.>')
+        else:
+            return xname
+sock.sendto(f'{choise_name()}'.encode('utf-8'), server) # отправляем имя на сервак
+
+def inputt(): #просто функция ввода
+    return input('Вы: ')
+
+while True: # проверка на уникальность никнейма
+        data = sock.recv(1024)
+        data = str(data.decode('utf-8'))
+        if data == '-1':
+            print('\nНикнейм занят, пожалуйста, введите другой! \n')
+            sock.sendto(f'{choise_name()}'.encode('utf-8'), server)
+        else:
+            print('\n' + data)
+            break
+
+print('Если хотите увидеть список комманд, введите -comms')
+sleep(1)
+
+global thread_stop #попытка научиться стопорить поток
+
+def read_msg(): #функция для чтения входящих сообщений
+    data = sock.recv(1024)
+    data = str(data.decode('utf-8'))
+    print('\n' + data)
+
+thread = threading.Thread(target=read_msg)#через потоки запускаем функцию
+thread.start()
+
+while True: #функция считывания ввода и аргументов для клиента 
+    sleep(0.5)
+    messege = inputt()
+    if messege == '-q':
+        sock.sendto('-q'.encode('utf-8'), server)
+        break
+    elif messege == '-comms':
+        print('\nСписок комманд и флагов: \n/users? - Пользователи онлайн \n$ - Сообщение всем \n-to xname - личное сообщение пользователю \n')
+    else:
+        sock.sendto(f'{messege}'.encode('utf-8'), server)
+
+sock.close()# закрывем сокет
+thread_stop = True #попытка закрыть поток
---
+++ program/server.py
@@ -0,0 +1,64 @@
+import socket
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #ставим сокет
+sock.bind (('127.0.0.1',4321)) #прописываем адрес сервера
+xnames_adds = {} #создаём словарь для хранения адресов и ников
+print ('Start Server')
+
+while 1:
+    data, addres = sock.recvfrom(1024) #принимаем сообщение с адресом отправления
+    data = str(data.decode('utf-8')) #дешифруем само сообщение
+    print(data, addres)
+    if (('$' in data) and ('connection users?' in data)) or ('connection users?' in data) and ('-to ' in data) or ('$' in data) and ('-to ' in data): #проверка пользователя на адекватность
+        sock.sendto('Пожалуйста, проверьте правильность написания флагов и их наличие'.encode('utf-8'), addres)
+    if addres not in xnames_adds.keys(): #проверяем, есть ли такой такой пользователь
+        print("prof")
+        if (data in xnames_adds.values()) and (addres not in xnames_adds.keys()): # проверка на уникальность никнейма
+            sock.sendto("-1".encode('utf-8'), addres) 
+        else:
+            if len(xnames_adds) == 0:                                           #
+                sock.sendto("Welcome, u're first))".encode('utf-8'), addres)    #
+            else:                                                               # говорим пользователю, об успешном доступе на сервер
+                sock.sendto("Welcome!".encode('utf-8'), addres)                 #
+            xnames_adds.update({addres:data}) # Добавляем пользователя в словарь
+            print(f'{data} Connect to server!')
+    elif data == '-q':                                      #
+        print(f'{xnames_adds.get(addres)}, disconnect!')    # если пользователь желает выйти, удаляем его из словаря
+        del xnames_adds[addres]                             #
+    elif '$' in data[0]:        # проверка флага на ширик
+        data = data[2::]
+        for client in xnames_adds.keys(): # в цикле отправляем всем клиентам
+            if client == addres:
+                continue
+            else:
+                sock.sendto((f'From {xnames_adds.get(addres)}: {data} (all)').encode('utf-8'), client)
+    elif '/users?' == data: #проверка на команду выдачи пользователей в сети
+        usrs = ''
+        for i in xnames_adds.values(): # избавляемся от лишнего синтаксиса
+            usrs = usrs + f'{i} \n' 
+        sock.sendto((f'Users: \n{usrs}').encode('utf-8'), addres)
+    elif '-to ' in data: # проверка на флаг личной отправки
+        s = list(data.split(' '))
+        print(s)
+        if s[1] not in xnames_adds.values(): # проверка на существование пользователя
+            sock.sendto(('Пользователь не найден(').encode('utf-8'), addres)
+        print(s[1], addres)
+        if xnames_adds.get(addres) == s[1]: # проверка на отправление самому себе
+            print('oaoao')
+            sock.sendto('Тебе настолько одиноко? \nСходи прогуляйся, че ты здесь торчишь?'.encode('utf-8'), addres)
+        else:
+            for clin in xnames_adds.keys(): # поиск адреса отправки
+                if xnames_adds.get(clin) == s[1]:
+                    s = s[2::]
+                    data = ''.join(map(str, s))#пересобираем сообщение в комфортный вид
+                    sock.sendto((f'From {xnames_adds.get(addres)}: {data} (personal)').encode('utf-8'), clin)
+                    break # на всякий, чтоб небыло выхода за цикл
+    elif ('down server' in data) and (xnames_adds.get(addres) == 'serveradmin'): #проверка на адекватность администратора сервера
+        print('Server down, thks admin)')
+        break
+    else:
+        sock.sendto('Пожалуйста, проверьте правильность написания флагов и их наличие)'.encode('utf-8'), addres)
+
+for i in xnames_adds.keys(): #отправка сообщений об отключении сервера
+    sock.sendto('Всем спасибо, сервер лёг)'.encode('utf-8'), i)
+sock.close()
+#down server - положить сервак(доступно не всем)
